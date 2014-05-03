c1975f3a8f05b3c0f338f12cab100a6d0d949371
Noon Silk <noonsilk@gmail.com>
2014-05-03 16:38:23 +1000

    few more people

--- src/_site/js/voronoi_links.js
+++ src/_site/js/voronoi_links.js
@@ -233,6 +233,9 @@ var VoronoiLinks = {
         links = [
             {"name": "Shtetl-Optimized", "link": "http://www.scottaaronson.com/blog/"},
             {"name": "Chris Done", "link": "http://www.chrisdone.com"},
+            {"name": "Thomas Viddick", "link": "http://mycqstate.wordpress.com/"},
+            {"name": "Quantum Frontiers", "link": "http://quantumfrontiers.com/"},
+            {"name": "The Quantum Pontiff", "link": "http://dabacon.org/pontiff"},
             {"name": "jehosafet", "link": "http://www.jehosafet.com/"},
             {"name": "Edwin Brady", "link": "http://edwinb.wordpress.com/blog/"},
             {"name": "Tony Morris", "link": "http://tmorris.net/"},
