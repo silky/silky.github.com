187542c0ef0132e4dd851201d06ba10fd0e48f20
silky <noonsilk+github@gmail.com>
2016-02-10 08:38:21 +1100

    Add CV
    Added a link to my resume.

--- src/site.hs
+++ src/site.hs
@@ -25,6 +25,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "files/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "js/*" $ do
         route   idRoute
         compile copyFileCompiler
